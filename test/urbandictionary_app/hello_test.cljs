6375b31849d53f68b1e9b5a530a9db2d7371f573
apeckham <apeckham@gmail.com>
2017-09-19 15:04:09 -0700

    test initialize-db

--- test/urbandictionary_app/hello_test.cljs
+++ test/urbandictionary_app/hello_test.cljs
@@ -3,13 +3,16 @@
             [re-frame.registrar :as registrar]
             [re-frame.core :as rf]
             re-frame.db
-            [urbandictionary-app.handlers :as handlers]))
+            [urbandictionary-app.handlers :as handlers]
+            [urbandictionary-app.db :as db]))
 
 (deftest my-test
          (is (= 2 2)))
 
 (deftest set-greeting-test
          (rf/dispatch-sync [:initialize-db])
+         (is (= db/app-db @re-frame.db/app-db))
+
          (rf/dispatch-sync [:set-greeting "hi mom"])
          (is (= "hi mom" (:greeting @re-frame.db/app-db))))
 
