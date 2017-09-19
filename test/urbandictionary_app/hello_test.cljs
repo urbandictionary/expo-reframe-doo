8cb24c43ce3638d89ccb5af1c1175010d440ce95
apeckham <apeckham@gmail.com>
2017-09-19 15:03:08 -0700

    add a test for set-greeting

--- test/urbandictionary_app/hello_test.cljs
+++ test/urbandictionary_app/hello_test.cljs
@@ -1,5 +1,15 @@
 (ns urbandictionary-app.hello-test
-  (:require [cljs.test :refer-macros [deftest is testing run-tests]]))
+  (:require [cljs.test :refer-macros [deftest is testing run-tests]]
+            [re-frame.registrar :as registrar]
+            [re-frame.core :as rf]
+            re-frame.db
+            [urbandictionary-app.handlers :as handlers]))
 
 (deftest my-test
          (is (= 2 2)))
+
+(deftest set-greeting-test
+         (rf/dispatch-sync [:initialize-db])
+         (rf/dispatch-sync [:set-greeting "hi mom"])
+         (is (= "hi mom" (:greeting @re-frame.db/app-db))))
+
