c95c50275dab0dd9c017369d6cf03dd5e9a1daf1
apeckham <apeckham@gmail.com>
2017-09-19 12:03:03 -0700

    show some random words from UD api

--- src/urbandictionary_app/core.cljs
+++ src/urbandictionary_app/core.cljs
@@ -1,23 +1,28 @@
 (ns urbandictionary-app.core
-    (:require [reagent.core :as r :refer [atom]]
-              [re-frame.core :refer [subscribe dispatch dispatch-sync]]
-              [urbandictionary-app.handlers]
-              [urbandictionary-app.subs]
-              [cljs-exponent.reagent :refer [text view image touchable-highlight] :as rn]))
+  (:require [reagent.core :as r :refer [atom]]
+            [re-frame.core :refer [subscribe dispatch dispatch-sync]]
+            [urbandictionary-app.handlers]
+            [urbandictionary-app.subs]
+            [cljs-exponent.reagent :refer [text view image touchable-highlight] :as rn]))
 
 (defn alert [title]
   (.alert rn/alert title))
+
+(defn pressed []
+  (-> (js/fetch "http://api.urbandictionary.com/v0/random")
+      (.then #(.json %))
+      (.then #(dispatch [:set-greeting (str (map :word (:list (js->clj % :keywordize-keys true))))]))))
 
 (defn app-root []
   (let [greeting (subscribe [:get-greeting])]
     (fn []
       [view {:style {:flex-direction "column" :margin 40 :align-items "center"}}
        [image {:source (js/require "./assets/images/cljs.png")
-               :style {:width 200
-                       :height 200}}]
+               :style  {:width  200
+                        :height 200}}]
        [text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}} @greeting]
-       [touchable-highlight {:style {:background-color "#999" :padding 10 :border-radius 5}
-                             :on-press #(alert "HELLO!")}
+       [touchable-highlight {:style    {:background-color "#999" :padding 10 :border-radius 5}
+                             :on-press pressed}
         [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "press me"]]])))
 
 (defn init []
